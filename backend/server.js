c5535b2a862628cd88d012c1399e56b4440d5b9e
ak7270090 <ak7270090@gmail.com>
2023-12-25 15:31:15 +0530

    cors error solved

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.listen(PORT,console.log(`server started on port ${PORT}`));
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-      origin: "http://localhost:3000",
+      origin: "*",
       // credentials: true,
     },
   });
